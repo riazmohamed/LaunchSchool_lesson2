d6148a7c48fecb7ee558907bb5f9eef44722ffda
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-19 22:10:43 +0000

    minor fix

--- car_loan_calculator.rb
+++ car_loan_calculator.rb
@@ -1,26 +1,24 @@
 # car_loan_calculator.rb
 
-=begin
-=> required information
-1. the loan amount
-2. the Annua Percentage Rate (APR)
-3. the loan duration
-
-=> from the above the following information needs to be calculated
-1. monthly interest rate
-2. loan duration in months
-
-Given formulae for calculation =====> m = p * (j / (1 - (1 + J) ** (-n)))
-=> m = monthly payment
-=> p = loan amount
-=> j = monthly interest rate
-=> n = loan duration in months
-=end
+# => required information
+# 1. the loan amount
+# 2. the Annua Percentage Rate (APR)
+# 3. the loan duration
+#
+# => from the above the following information needs to be calculated
+# 1. monthly interest rate
+# 2. loan duration in months
+#
+# Given formulae for calculation =====> m = p * (j / (1 - (1 + J) ** (-n)))
+# => m = monthly payment
+# => p = loan amount
+# => j = monthly interest rate
+# => n = loan duration in months
 
 # Pseudo code
 # - get the amount of loann needed from the user
 # - display the monthly interest rate as %5
-# - prompt the user to select 36, 42 or 60 month repayment for the capital along with the interest
+# - prompt the user to select 36, 42 or 60 month repayment
 # - based on the above info calculate the monthly payment for the agreed term.
 # - print the details
 
@@ -28,7 +26,7 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_name?(name)      # condition to use strings for names
+def valid_name?(name) # condition to use strings for names
   if name.to_i.to_s != '0'
     false
   elsif name == '0'
@@ -52,7 +50,9 @@ end
 
 # m = p * (j / (1 - (1 + j)**(-n)))
 def loan_calculation(amount_borrowed, loan_duration)
-  monthly_payment = amount_borrowed.to_f * ((0.05/12) / (1 - (1 + (0.05/12))**(-loan_duration.to_i)))
+  monthly_payment = amount_borrowed.to_f *
+                    ((0.05 / 12) /
+                    (1 - (1 + (0.05 / 12))**-loan_duration.to_i))
   monthly_payment
 end
 
@@ -94,19 +94,22 @@ prompt(loan_term_prompt)
 
 loan_term = gets.chomp
 
-result = case loan_term
+case loan_term
 when '1'
   prompt("You have chosen 36 months loan term")
   loan_duration = 36
-  puts "Your monthly payment for 36 months is $#{loan_calculation(loan_amount,loan_duration)}"
+  puts "Your monthly payment for 36 months is
+        $#{loan_calculation(loan_amount, loan_duration)}"
 when '2'
   prompt("You have chosen 42 months loan term")
   loan_duration = 42
-  puts "Your monthly payment for 42 months is $#{loan_calculation(loan_amount, loan_duration)}"
+  puts "Your monthly payment for 42 months is
+        $#{loan_calculation(loan_amount, loan_duration)}"
 when '3'
   prompt("You have chosen 60 months loan term")
   loan_duration = 60
-  puts "Your monthly payment for 60 months is $#{loan_calculation(loan_amount, loan_duration)}"
+  puts "Your monthly payment for 60 months is
+        $#{loan_calculation(loan_amount, loan_duration)}"
 else
   prompt("Please make a valid selection 1) 2) or 3)")
 end
