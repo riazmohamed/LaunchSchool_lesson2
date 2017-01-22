86ecba19a98885ba96dc21b5d22a75e55c764654
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-22 05:26:13 +0000

    Add lizard, spock . Add shorthand. Add scoring system

--- rps_bonus_features.rb
+++ rps_bonus_features.rb
@@ -1,12 +1,13 @@
 # rps_bonus_features.rb
 
 # Pseudo code
- # if the player wins then his score is +1
- # else if the compter wins then computers score is +1
- # else if it is a tie then score = +0 for both
- #
+# - if the player wins then his score is +1
+# - else if the compter wins then computers score is +1
+# - else if it is a tie then score = +0 for both
 
 # 1. Add Lizard and Spock into your code
+
+# 3. keep score of how many times computer has won
 
 VALID_CHOICES = %w(rock paper scissors lizard spock)
 
@@ -27,27 +28,50 @@ def win?(player1, player2)
     (player1 == 'spock' && player2 == 'paper') # spock burns paper
 end
 
-player_score = 0
-computer_score = 0
 def display_results(player, computer)
   if win?(player, computer)
     prompt("You won!")
-    player_score = player_score + 1
   elsif win?(computer, player)
     prompt("Computer won!")
-    computer_score = computer_score + 1
   else
     prompt("It's a tie!")
   end
 end
 
+player_score = 0
+computer_score = 0
 loop do
   choice = ''
+  player_input = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
+    choose_option = <<-MSG
+    Please select from one of the following options
+    Type 'r' for 'rock'
+    Type 'p' for 'paper'
+    Type 'sc' for 'scissors'
+    Type 'l' for 'lizard'
+    Type 'sp' for 'spock'
+    MSG
 
-    if VALID_CHOICES.include?(choice)
+    # prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt(choose_option)
+    player_input = Kernel.gets().chomp()
+
+    choice = case player_input
+             when 'r'
+               VALID_CHOICES[0]
+             when 'p'
+               VALID_CHOICES[1]
+             when 'sc'
+               VALID_CHOICES[2]
+             when 'l'
+               VALID_CHOICES[3]
+             when 'sp'
+               VALID_CHOICES[4]
+             end
+
+    if VALID_CHOICES.include?(choice.downcase)
+      prompt("#{choice} selected!")
       break
     else
       prompt("Seems like something is not right! Make a valid selection")
@@ -59,9 +83,20 @@ loop do
   display_results(choice, computer_choice)
 
   prompt("You chose : '#{choice}', computer chose : '#{computer_choice}'")
+
+  if win?(choice, computer_choice)
+    player_score += 1
+    player_score
+  elsif win?(computer_choice, choice)
+    computer_score += 1
+    computer_score
+  else
+    player_score
+    computer_score
+  end
   prompt("Score - You: #{player_score}, Computer: #{computer_score}")
 
-  prompt("Do you want to play again?")
+  prompt("Do you want to play again? (y/n)")
   answer = Kernel.gets().chomp()
   break unless answer.downcase.start_with?('y')
 end
